30f64b6de7812d4841a7d66443da648d5b4d2866
Sergey <7maxi.ss@gmail.com>
2023-09-08 20:12:40 +0500

    Add throw exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 class Main {
@@ -16,26 +17,46 @@ class Main {
             "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
             "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
             "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
-    public static void main(String[] args){
-        System.out.println(calc());
-    }
 
-    public static String calc() {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String inputResult = scanner.nextLine();
-        if (inputResult.split(" ").length != 3) return ("Ошибка ввода");
-        testRoman(inputResult);
+        System.out.println(calc(inputResult));
+    }
+
+
+    public static String calc(String input) throws Exception {
+
+        if (input.split(" ").length != 3) throw new Exception("т.к. формат математической операции " +
+                "не удовлетворяет заданию" +
+                " - два операнда и один оператор (+, -, /, *)");
+        testRoman(input);
 
         if (firstNumberIsArabian && secondNumberIsArabian) {
-            return startCalcArab(inputResult);
+            return startCalcArab(input);
         } else if (firstNumberIsArabian || secondNumberIsArabian) {
-            return "Неудовлетворяет условию";
+            throw new Exception("т.к. используются одновременно разные системы счисления");
         } else {
-            return startCalcRoman(inputResult);
+            return startCalcRoman(input);
         }
     }
 
-    static String startCalcRoman(String input) {
+    static void testRoman(String input) {
+        String[] tokkens = input.split(" ");
+        for (int i = 0; i < romanNumbers.length; i++) {
+            if (tokkens[0].equals(romanNumbers[i])) {
+                firstNumberIsArabian = false;
+            }
+        }
+        for (int j = 0; j < romanNumbers.length; j++) {
+            if (tokkens[2].equals(romanNumbers[j])) {
+                secondNumberIsArabian = false;
+            }
+        }
+
+    }
+
+    static String startCalcRoman(String input) throws Exception {
         String[] tokkens = input.split(" ");
         int a = 0;
         int b = 0;
@@ -57,7 +78,7 @@ class Main {
                 break;
             case "-":
                 if (a < b) {
-                    return "Нет отрицательных числе в Римской системе";
+                    throw new Exception("Нет отрицательных числе в Римской системе");
 
                 } else {
                     result = a - b;
@@ -70,35 +91,19 @@ class Main {
                 result = a / b;
                 break;
             default:
-                return "Неизвестный знак";
+                throw new Exception("Неизвестный знак");
         }
-        if (result == 0) return "В Римской системе нет нуля";
+        if (result == 0) throw new Exception("В Римской системе нет нуля");
         return (romanTypeNumbers[result - 1]);
     }
 
-    static void testRoman(String input) {
-        String[] tokkens = input.split(" ");
-        for (int i = 0; i < romanNumbers.length; i++) {
-            if (tokkens[0].equals(romanNumbers[i])) {
-                firstNumberIsArabian = false;
-            }
-        }
-        for (int j = 0; j < romanNumbers.length; j++) {
-            if (tokkens[2].equals(romanNumbers[j])) {
-                secondNumberIsArabian = false;
-            }
-        }
-
-    }
-
-
-    static String startCalcArab(String input) {
+    static String startCalcArab(String input) throws Exception {
         String[] tokkens = input.split(" ");
         try {
             int a = Integer.parseInt(tokkens[0]);
             int b = Integer.parseInt(tokkens[2]);
             if ((a < 1 || a > 10) || (b < 1 || b > 10)) {
-                return "Переменные больше 10 или меньше 1";
+                throw new IOException("Переменные больше 10 или меньше 1");
             } else {
                 String operator = tokkens[1];
                 int result = 0;
@@ -116,12 +121,13 @@ class Main {
                         result = a / b;
                         break;
                     default:
-                        return "Неизвестный знак";
+                        throw new IOException("Неизвестный оператор");
                 }
                 return Integer.toString(result);
             }
-        } catch (Exception e) {
-            return "Ошибка при вводе данных";
+        } catch (RuntimeException e){
+           throw new Exception("Неизвестное значение одного из чисел");
         }
+
     }
 }
